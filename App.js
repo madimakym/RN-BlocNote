f05dc84fc3f21c1a3ba853e5287ceabc9e1d34f6
Maky Madi <mmadingou@peopleinput.com>
2019-01-25 13:17:05 +0000

    delete splashScreen

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import Main from './app/Main';
-import SplashScreen from 'react-native-splash-screen';
+// import SplashScreen from 'react-native-splash-screen';
 
 
 export default class App extends React.Component {
-	componentDidMount() {
+	// componentDidMount() {
 
-		// SplashScreen.hide()
-		setTimeout(() => SplashScreen.hide(), 1000);
+	// 	// SplashScreen.hide()
+	// 	// setTimeout(() => SplashScreen.hide(), 1000);
 	
-	  }
+	//   }
 
 	render() {
 		return <Main />;
